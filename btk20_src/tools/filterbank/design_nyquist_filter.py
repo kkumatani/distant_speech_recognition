087ed0d6ead9e93e86b0566ee079a60112f0d5b1
pf <pf@pf-ubuntu-dev>
2020-08-28 14:00:00 +0100

    - use np as numpy - making filter generation in python a billion times faster

--- btk20_src/tools/filterbank/design_nyquist_filter.py
+++ btk20_src/tools/filterbank/design_nyquist_filter.py
@@ -7,8 +7,7 @@ Kenichi Kumatani, John McDonough, Stefan Schacht, Dietrich Klakow, Philip N Garn
 
 .. moduleauthor:: Kenichi Kumatani <k_kumatani@ieee.org>
 """
-import numpy
-import numpy.linalg
+import numpy as np
 import os
 import pickle
 
@@ -25,17 +24,17 @@ def mynull(A, num=0, datatype='d'):
     :returns: null space projection matrix of A and singular weights
     """
 
-    [U,W,VH] = numpy.linalg.svd(A)
+    [U,W,VH] = np.linalg.svd(A)
     V = VH.transpose()
     (rowN, colN) = A.shape
     if num > 0:
         sX = colN - num
-        val  = numpy.zeros(num, numpy.float)
+        val  = np.zeros(num, np.float)
     else:
         if rowN > 1:
-            s = numpy.diag(W)
+            s = np.diag(W)
         elif rowN == 1:
-            s = numpy.array([[W[0]]])
+            s = np.array([[W[0]]])
 
         if datatype == 'd': # double precision accuracy
             tol = max(rowN, colN) * s.max() * 2.2204e-16
@@ -43,10 +42,10 @@ def mynull(A, num=0, datatype='d'):
             tol = max(rowN, colN) * s.max() * 1.1921e-07
 
         print('Threshold for nullspace: %e' %tol)
-        sX = numpy.sum(s  > tol)
-        val = numpy.zeros(colN-sX, numpy.float)
+        sX = np.sum(s  > tol)
+        val = np.zeros(colN-sX, np.float)
 
-    y = numpy.array(V[:, sX:colN:1])
+    y = np.array(V[:, sX:colN:1])
     for i in range(len(val)):
         val[i] = W[sX+i]
 
@@ -68,35 +67,35 @@ def design_Nyquist_analyasis_filter_prototype(M, m, D, wpW=1):
     L_h   = M * m   # length of the prototype filter
     md    = L_h / 2 if m != 1 else 0 # group delay offset
     tau_h = L_h / 2 # group delay of analysis fb
-    w_p   = numpy.pi / (wpW * M)  # passband cut-off frequency
+    w_p   = np.pi / (wpW * M)  # passband cut-off frequency
 
-    A = numpy.zeros((L_h, L_h), numpy.float) # A is (L_h x L_h) hermitian matrix
-    b = numpy.zeros((L_h, 1),   numpy.float)
-    C = numpy.zeros((L_h, L_h), numpy.float)
+    i = np.arange(0, L_h)
+    j = np.arange(0, L_h)
+    i = np.expand_dims(i, -1)
+    j = np.expand_dims(j, 0)
+    j_i = j - i
 
-    for i in range(L_h):
-        for j in range(L_h):
-            factor = float(D - 1) if ((j - i) % D) == 0 else -1.0
+    factor = np.where(j_i % D == 0, D - 1, -1.0)
 
-            if (j - i) == 0:
-                C[i][j] = factor / D
-            else:
-                C[i][j] = factor * (numpy.sin(numpy.pi * (j - i) / D)) / (numpy.pi * (j - i))
+    C = np.where(j_i == 0,
+                 factor / D,
+                 factor * np.sin(np.pi * j_i / D) / (np.pi * j_i)
+                 )
 
-            if (j - i) == 0:
-                A[i][j] = 1.0
-            else:
-                A[i][j] = numpy.sin(w_p * (j - i)) / (w_p * (j - i))
+    A = np.where(j_i == 0,
+                 1.0,
+                 np.sin(w_p * j_i) / (w_p * j_i)
+                 )
 
-        if (tau_h - i) == 0:
-            b[i] = 1.0
-        else:
-            b[i] = numpy.sin(w_p * (tau_h - i)) / (w_p * (tau_h - i))
+    b = np.where((tau_h - i) == 0,
+                 1.0,
+                 np.sin(w_p * (tau_h - i)) / (w_p * (tau_h - i))
+                 )
 
     # delete the rows and columns of C corresponding to the components of h = 0
-    delC = numpy.zeros((L_h - m + 1, L_h - m + 1), numpy.float)
-    delA = numpy.zeros((L_h - m + 1, L_h - m + 1), numpy.float)
-    delb = numpy.zeros((L_h - m + 1, 1),           numpy.float)
+    delC = np.zeros((L_h - m + 1, L_h - m + 1), np.float)
+    delA = np.zeros((L_h - m + 1, L_h - m + 1), np.float)
+    delb = np.zeros((L_h - m + 1, 1),           np.float)
     i = 0
     for k in range(L_h):
         if k == md or (k % M) != 0:
@@ -110,12 +109,12 @@ def design_Nyquist_analyasis_filter_prototype(M, m, D, wpW=1):
             delb[i] = b[k]
             i += 1
 
-    rank_delC = numpy.linalg.matrix_rank(delC)
+    rank_delC = np.linalg.matrix_rank(delC)
     if rank_delC == len(delC):
         # take an eigen vector corresponding to the smallest eigen value.
-        eVal, eVec = numpy.linalg.eig(delC)
+        eVal, eVec = np.linalg.eig(delC)
         # take eigen vectors as basis
-        minX = numpy.argmin(eVal)
+        minX = np.argmin(eVal)
         print('nmin eigen val: {}'.format(eVal[minX]))
         rh = eVec[:,minX]; # eigen values are sorted in the ascending order.
         # flip the sign if all the coefficients are negative
@@ -132,18 +131,18 @@ def design_Nyquist_analyasis_filter_prototype(M, m, D, wpW=1):
         print( 'No. null space bases of C is %d' %len(nulldelC[0]))
         # In general, null(delP) is not a square matrix.
         # We don't want to use a peseude inversion matrix as much as possible.
-        T1    = numpy.dot(delA, nulldelC)
-        T1_2  = numpy.dot(nulldelC.transpose(), T1)
-        rank_T = numpy.linalg.matrix_rank(T1_2)
+        T1    = np.dot(delA, nulldelC)
+        T1_2  = np.dot(nulldelC.transpose(), T1)
+        rank_T = np.linalg.matrix_rank(T1_2)
         if rank_T == len(T1_2):
-            x = numpy.linalg.solve(T1_2, numpy.dot(nulldelC.transpose(), delb))
+            x = np.linalg.solve(T1_2, np.dot(nulldelC.transpose(), delb))
         else:
             print('Use pseudo-inverse matrix because %d < %d' %(rank_T, len(T1_2)))
-            x = numpy.dot(linalg.pinv(T1), delb)
-        rh = numpy.dot(nulldelC, x)
+            x = np.dot(np.linalg.pinv(T1), delb)
+        rh = np.dot(nulldelC, x)
 
     # re-assemble the complete prototype
-    h = numpy.zeros((L_h, 1), numpy.float)
+    h = np.zeros((L_h, 1), np.float)
     k = 0
     for m in range(L_h):
         if m != md and (m % M) == 0:
@@ -153,9 +152,9 @@ def design_Nyquist_analyasis_filter_prototype(M, m, D, wpW=1):
             k += 1
 
     # Pass-band error: h' * A * h - 2 * h' * b + 1
-    # alpha = numpy.dot(h.transpose(), numpy.dot(A, h)) - 2 * numpy.dot(h.transpose(), b) + 1
+    # alpha = np.dot(h.transpose(), np.dot(A, h)) - 2 * np.dot(h.transpose(), b) + 1
     # Inband aliasing distortion: h' * C * h
-    beta  = numpy.dot(h.transpose(), numpy.dot(C, h))
+    beta  = np.dot(h.transpose(), np.dot(C, h))
 
     return (h, beta)
 
@@ -180,11 +179,11 @@ def design_Nyquist_synthesis_filter_prototype(h, M, m, D, wpW=1):
     md    = L_h / 2 if m != 1 else 0
     tau_g = L_g / 2 # group delay of synthesis fb
     tau_t = md + tau_g # total filterbank delay
-    w_p   = numpy.pi / (wpW * M)  # cut-off frequency
+    w_p   = np.pi / (wpW * M)  # cut-off frequency
 
-    E = numpy.zeros((L_g, L_g), numpy.float)
-    f = numpy.zeros((L_g, 1),   numpy.float)
-    P = numpy.zeros((L_g, L_g), numpy.float)
+    E = np.zeros((L_g, L_g), np.float)
+    f = np.zeros((L_g, 1),   np.float)
+    P = np.zeros((L_g, L_g), np.float)
 
     for i in range(L_g):
         for j in range(L_g):
@@ -202,13 +201,13 @@ def design_Nyquist_synthesis_filter_prototype(h, M, m, D, wpW=1):
             f[i] = h[tau_t-i][0]
 
     E = ((M * M) / float(D / D)) * E
-    f = (M / (numpy.pi * D)) * f
+    f = (M / (np.pi * D)) * f
     P = (M / float(D * D)) * P
 
     # Shift a time-reversed version of h and make a matrix.
     # The k-th row of the matrix indicates h_k
     rowN = 2 * m - 1;
-    H  = numpy.zeros((rowN, L_g), numpy.float ) # a row vector corresponds to h_k in the report
+    H  = np.zeros((rowN, L_g), np.float ) # a row vector corresponds to h_k in the report
     sX = M
     eX = sX - L_g + 1
     for i in range(rowN):
@@ -222,38 +221,38 @@ def design_Nyquist_synthesis_filter_prototype(h, M, m, D, wpW=1):
             e = 1
         elif e > L_g:
             e = L_g
-        H[i, e-1:s] = numpy.array([h[j-1, 0] for j in range(s,e-1,-1)])
+        H[i, e-1:s] = np.array([h[j-1, 0] for j in range(s,e-1,-1)])
         sX += M
         eX += M
 
-    C0 = numpy.zeros((rowN, 1), numpy.float)
+    C0 = np.zeros((rowN, 1), np.float)
     C0[m-1][0] = D * 1.0 / M # C0(m) = h(md+1);
 
     sizeP = len(P)
-    rank_P = numpy.linalg.matrix_rank(P);
+    rank_P = np.linalg.matrix_rank(P);
     if rank_P == sizeP:
         print('Use Lagrange multiplier...')
-        invP = numpy.linalg.inv( P );
-        H_invP_HT = numpy.dot(numpy.dot(H, invP), H.transpose())
-        g = numpy.dot(numpy.dot(numpy.dot(invP, H.transpose()), numpy.linalg.inv(H_invP_HT)), C0)
+        invP = np.linalg.inv( P );
+        H_invP_HT = np.dot(np.dot(H, invP), H.transpose())
+        g = np.dot(np.dot(np.dot(invP, H.transpose()), np.linalg.inv(H_invP_HT)), C0)
     elif rank_P <= (sizeP - rowN):
         print('Use the null space...')
         nullP, _w = mynull(P)
         print('No. null space bases of P is %d' %len(nullP[0]))
-        y = numpy.dot(numpy.linalg.pinv(numpy.dot(H, nullP)), C0)
-        g = numpy.dot(nullP, y)
+        y = np.dot(np.linalg.pinv(np.dot(H, nullP)), C0)
+        g = np.dot(nullP, y)
     else:
         # will not find enough bases of the null space
         print('Use SVD (rank(P)=%d)...' %rank_P)
-        [UP,WP,VP] = numpy.linalg.svd( P );
+        [UP,WP,VP] = np.linalg.svd( P );
         pnullP = VP[:,(sizeP-rowN):sizeP]
-        y = numpy.linalg.solve(numpy.dot(H, pnullP), C0)
-        g = numpy.dot(pnullP, y)
+        y = np.linalg.solve(np.dot(H, pnullP), C0)
+        g = np.dot(pnullP, y)
 
     # Total response error: g' * E * g - 2 * g' * f + 1
-    # gamma = numpy.dot(g.transpose(), numpy.dot(E, g)) - 2 * numpy.dot(g.transpose(), f) + 1
+    # gamma = np.dot(g.transpose(), np.dot(E, g)) - 2 * np.dot(g.transpose(), f) + 1
     # Residual aliasing distortion: g' * P * g
-    epsir = numpy.dot(g.transpose(), numpy.dot(P, g))
+    epsir = np.dot(g.transpose(), np.dot(P, g))
 
     return (g, epsir)
 
@@ -264,13 +263,13 @@ def main(M, m, r, outputdir):
         D = 1
 
     (h, beta) = design_Nyquist_analyasis_filter_prototype(M, m, D)
-    print('Inband aliasing error: %f dB' %(10*numpy.log(beta)))
+    print('Inband aliasing error: %f dB' %(10*np.log(beta)))
     analysis_filename = os.path.join(outputdir, 'h-M%d-m%d-r%d.pickle' %(M, m, r))
     with open(analysis_filename, 'wb') as hfp:
         pickle.dump(h.flatten(), hfp, True)
 
     (g, epsir) = design_Nyquist_synthesis_filter_prototype(h, M, m, D)
-    print('Residual aliasing distortion: %f dB' %(10*numpy.log(epsir)))
+    print('Residual aliasing distortion: %f dB' %(10*np.log(epsir)))
     synthesis_filename = os.path.join(outputdir, 'g-M%d-m%d-r%d.pickle' %(M, m, r))
     with open(synthesis_filename, 'wb') as gfp:
         pickle.dump(g.flatten(), gfp, True)
